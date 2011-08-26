68479eaad454151d4df532db8a964f14935d8193
Blake Johnson <blake.johnson@byu.edu>
2011-08-26 09:34:51 -0600

    change bashrc as well

--- install.rkt
+++ install.rkt
@@ -7,11 +7,17 @@
 (define home-dir (find-system-path 'home-dir))
 (define home-emacs-el (build-path home-dir ".emacs.el"))
 (define home-profile (build-path home-dir ".profile"))
+(define home-bashrc (build-path home-dir ".bashrc"))
 
 (backup-file home-emacs-el)
 (backup-file home-profile)
+(backup-file home-bashrc)
 
 (make-file-or-directory-link (simplify-path emacs-el-path) home-emacs-el)
 
-(with-output-to-file home-profile (λ () (printf "export PATH=~a:$PATH\n" (simplify-path scripts-path))))
+(define (print-path)
+  (printf "export PATH=~a:$PATH\n" (simplify-path scripts-path)))
+
+(with-output-to-file home-profile print-path)
+(with-output-to-file home-bashrc print-path)
 
