4774934233972792713fc60f7f8efea91e317cbe
Blake Johnson <blake.johnson@byu.edu>
2011-08-26 13:50:52 -0600

    up

---
+++ get-graded.rkt
@@ -0,0 +1,29 @@
+#lang racket/base
+(require racket/cmdline
+         racket/match
+         "data.rkt"
+         "compile.rkt")
+
+(define-values (dir num optional)
+  (command-line
+   #:program "turnin"
+   #:args (assignment)
+   (match assignment
+     [(regexp #rx"^([0-9]+)(opt)?$" (list dir num opt))
+      (values dir (string->number num) (not (not opt)))]
+     [else 
+      (printf "~a is not a valid assignment\n" assignment)
+      (exit)])))
+
+(define turnin-dir (build-path current-student-dir dir))
+
+(unless (assignment-graded? turnin-dir)
+  (printf "Assignment ~a hasn't been graded yet.\n" dir)
+  (exit))
+
+(define graded-dir (format "graded-~a-~a" dir (current-seconds)))
+(copy-directory/files turnin-dir graded-dir)
+(for ([file (in-directory graded-dir)])
+  (file-or-directory-permissions file #o311))
+
+(printf "Graded files for assignment ~a are in ~a\n" dir graded-dir)
